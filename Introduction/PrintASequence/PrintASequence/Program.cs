44286caa488e4801ed00ed62bb79d79d3e6a9827
Kambocha <kambocha7@gmail.com>
2015-01-09 18:56:23 +0200

    Commit 1

---
+++ Introduction/PrintASequence/PrintASequence/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintASequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 2; i <= 10; i++) {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+                else 
+                {
+                    Console.WriteLine(i * (-1));
+                }
+            }
+        }
+    }
+}
---
+++ Introduction/PrintLongSequence/PrintLongSequence/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintLongSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 2; i <= 1000; i++) {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+                else {
+                    Console.WriteLine(i * (-1));
+                }
+            }
+        }
+    }
+}
